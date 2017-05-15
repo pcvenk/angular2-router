9d05aefbeec37360fe3b59568eaa246551efa27e
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-05-15 11:09:54 +0200

    Added the 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -28,6 +29,7 @@ const appRoutes: Routes = [
       { path: ':id/:name', component: UserComponent }
     ]
   },
+  { path: '**', component: PageNotFoundComponent}
 
 ];
 
@@ -39,7 +41,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
