b50b1b5a72516e17defd40d5c202482d92a32874
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-05-15 17:59:06 +0200

    Added the app-router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes} from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -14,24 +14,8 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'servers', component: ServersComponent, children:
-    [
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
-    ]
-  },
-
-  { path: 'users', component: UsersComponent, children:
-    [
-      { path: ':id/:name', component: UserComponent }
-    ]
-  },
-  { path: '**', component: PageNotFoundComponent}
-
-];
 
 @NgModule({
   declarations: [
@@ -48,7 +32,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
