c17539be8a8cccf23e69ffe6f34a3b23e8565e1c
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-05-16 17:34:26 +0200

    Passing static data to the 404 page via the router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const appRoutes: Routes = [
       { path: ':id/:name', component: UserComponent }
     ]
   },
-  { path: '**', component: PageNotFoundComponent}
+  { path: '**', component: PageNotFoundComponent, data: {message: 'Page Not Found'}}
 ];
 
 @NgModule ({
--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  message: string;
+
+  constructor(private actRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.actRoute.data.subscribe(
+      (data) => {
+        this.message = data['message'];
+      }
+    )
   }
+
 
 }
