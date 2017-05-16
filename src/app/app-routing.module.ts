034acdd1fc9a15503e485e0fa2fa1e3f1f17d62f
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-05-16 10:24:08 +0200

    Adding the canDeactivate guard to the routing module and the app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,13 +9,14 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children:
     [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
     ]
   },
   { path: 'users', component: UsersComponent, children:
